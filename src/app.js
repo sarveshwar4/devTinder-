5e4a434c9e49b23faedbc08c5d6d3558ec36179b
Sarveshwar Shukla <sarveshwarkumarshukla@gmail.com>
2025-02-19 00:37:30 +0530

    Exploring get, post,delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 
 const app = express();
+// this will handle oly get Call to /user
+app.get('/user', (req, res) => {
+    console.log(req.params);
+    res.send({"firstName" : "Sarveshwar" , "LastName" : "Shukla"});
+});
 
-app.use((req, res)=>{
-     res.end("hello ji i am the sarveshwar")
+app.post('/user', (req, res)=>{
+    // data is stpring on the database
+      res.send("data is stored successfully to the database");
 })
 
-app.use('/hello',(req, res)=>{
-    res.end("hello ji")
+app.delete('/user',(req, res)=>{
+    res.send("Data Deleted from the database")
 })
-
 app.use('/test',(req, res)=>{
-    res.end("hello ji kaise ho aap sab")
+    res.send("hello ji kaise ho aap sab")
 })
 
-app.listen(7777);
+// this will handle all the upcoming request like get post delete patch every request wild card match '/'
+// app.use('/',(req, res)=>{
+//      res.send("hello ji i am the sarveshwar")
+// })
+
+app.listen(7777, ()=>{
+    console.log("Server is connected successfully")
+});
