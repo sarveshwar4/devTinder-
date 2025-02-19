62da2db437d04625f6da890aaccd309d6b44e9f9
Sarveshwar Shukla <sarveshwarkumarshukla@gmail.com>
2025-02-20 02:28:52 +0530

    Writing-auth-middleWare

--- src/app.js
+++ src/app.js
@@ -1,29 +1,28 @@
 const express = require('express');
 
 const app = express();
-// this will handle oly get Call to /user
-app.get('/user', (req, res) => {
-    console.log(req.params);
-    res.send({"firstName" : "Sarveshwar" , "LastName" : "Shukla"});
-});
 
-app.post('/user', (req, res)=>{
-    // data is stpring on the database
-      res.send("data is stored successfully to the database");
+app.use("/user" , (req,res, next)=>{
+   console.log("Handling the route user 01");;
+//    res.send("1st Response")
+   next();
+},
+ (req, res, next)=>{
+    console.log("Handling the route user 02");
+    // res.send("2nd Response")
+    next();
+},
+ (req, res,next)=>{
+    console.log("handling the route user 3rd");
+    // res.send("3rd Response")
+    next();
+},
+ (req, res, next)=>{
+    console.log("handling the route user 4th");
+    res.send("4th Response");
+    next();
 })
 
-app.delete('/user',(req, res)=>{
-    res.send("Data Deleted from the database")
+app.listen(7777,()=>{
+    console.log("Server is listining successfully on port 7777")
 })
-app.use('/test',(req, res)=>{
-    res.send("hello ji kaise ho aap sab")
-})
-
-// this will handle all the upcoming request like get post delete patch every request wild card match '/'
-// app.use('/',(req, res)=>{
-//      res.send("hello ji i am the sarveshwar")
-// })
-
-app.listen(7777, ()=>{
-    console.log("Server is connected successfully")
-});
