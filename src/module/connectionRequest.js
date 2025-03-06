0ad33845a07616de249daa315bb54be3b406a119
Sarveshwar Shukla <sarveshwarkumarshukla@gmail.com>
2025-03-07 01:16:50 +0530

    creating the request /send/:status/:userid api

--- src/module/connectionRequest.js
+++ src/module/connectionRequest.js
@@ -1,20 +1,34 @@
+const e = require("express");
 const mongoose  = require("mongoose");
 const connectionRequestSchema = mongoose.Schema({
     fromUserId : {
-        type : String,
+        type : mongoose.Schema.Types.ObjectId,
         required : true,
     },
     toUserId : {
-        type : String,
+        type : mongoose.Schema.Types.ObjectId,
         required : true,
     },
     status :{
         type : String,
-        validate(value){
-            if(!['interested', 'ignored', 'accepted', 'rejected'].includes(value)){
-                throw new Error('Enter the correct Status Type');
-            }
+        required : true,
+        enum : {
+            values : ['interested', 'ignored', 'accepted', 'rejected'],
+            message : `{VALUE} is incorrect status type`
         }
+    }  
+}, 
+ {timestamps : true}
+);
+
+connectionRequestSchema.pre("save", async function(next){
+    const request = this;
+    const fromUserid = request.fromUserId;
+    const toUserId = request.toUserId;
+    if(fromUserid.equals(toUserId)){
+        throw new Error("You can't send request to yourself");
     }
+    next();
 })
+
 module.exports = mongoose.model('connectionRequest', connectionRequestSchema);
--- src/routes/request.js
+++ src/routes/request.js
@@ -3,33 +3,33 @@ const requestRouter = express.Router();
 const {userAuth} = require("../middleware/auth");
 const connectionRequest = require("../module/connectionRequest");
 const User = require("../module/user")
-requestRouter.post("/send/request/:status/:userId",userAuth, async(req, res)=>{
+requestRouter.post("/request/send/:status/:userId",userAuth, async(req, res)=>{
       try{
         const user = req.user;
         const fromUserId = user._id;
         const toUserId = req.params.userId;
-
-        // sending request to ourself
-        if(fromUserId == toUserId){
-          throw new Error("You can't send request to yourself");
+        const status = req.params.status;
+        // checking the status is valid or not
+        const allowed_update = ['interested', 'ignored'];
+        if(!allowed_update.includes(status)){
+          res.status(400).send("Invalid status");
         }
+        // checking the request is already sent or not
         const userConnectedData = await connectionRequest.findOne({
           $or: [
             { fromUserId, toUserId },
             { fromUserId: toUserId, toUserId: fromUserId }
           ]
         });
-      //  checking the request is already sent or not
+      //checking the request is already sent or not
         if(userConnectedData){
           throw new Error("Request is Already Sent")
         }
-        // checking the user id is valid or not                   
+        // checking the user is valid or not to send the request
         const isValidatetoUserId = await User.findById({_id : toUserId});
         if(!isValidatetoUserId){
           throw new Error("id is not valid ")
         }
-        const status = req.params.status;
-  
         const connectionRequests = await new connectionRequest(
         //  creating the object of connectionRequest
           {
@@ -38,9 +38,9 @@ requestRouter.post("/send/request/:status/:userId",userAuth, async(req, res)=>{
             status
           }
         )
-        const connectionRequestSaved = await connectionRequests.save();
+        const data = await connectionRequests.save();
 
-        res.json({connectionRequestSaved});
+        res.json({message : `${user.firstName} request sent to ${isValidatetoUserId.firstName}`, data});
       }
       catch(error){
         res.status(400).send("ERROR:" + error.message);
