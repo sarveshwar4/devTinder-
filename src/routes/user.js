2762153fab7de0b168ea4997cd6ef9281cefab1f
Sarveshwar Shukla <sarveshwarkumarshukla@gmail.com>
2025-03-11 23:47:34 +0530

    Add user feed endpoint to retrieve filtered user data

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,8 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../module/connectionRequest");
-
+const User = require("../module/user");
+const user_safe_data = "firstName lastName email, photoUrl about skills";
 userRouter.get("/user/request/recieved", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
@@ -62,6 +63,43 @@ userRouter.get("/user/connection/view", userAuth, async (req, res) => {
     res.json({ data });
   } catch (error) {
     res.status(400).send("ERROR:" + error.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;  
+    let limit = parseInt(req.query.limit) || 10;  
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page-1)*limit;  
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    console.log(connectionRequest);
+    const hideUserFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId.toString());
+
+      hideUserFromFeed.add(req.toUserId.toString());
+    });
+    console.log(hideUserFromFeed);
+
+    const userFeed = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+    .select(user_safe_data)
+    .skip(skip)
+    .limit(limit);
+
+    res.json({ data: userFeed });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
   }
 });
 
