a146148871319d41686355070fbaa095b7d4fc8c
Sarveshwar Kumar Shukla <sarveshwarkumarshukla@gmail.com>
2025-06-26 01:41:21 +0530

    Update CORS origin to allow requests from the production frontend

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -6,7 +6,7 @@ const socketAuth = require("./socketAuth");
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://dev-tinder-web-chi.vercel.app",
       credentials: true,
     },
   });
