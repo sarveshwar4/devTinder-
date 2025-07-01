61a56b3e098c409c59fd079f5a869b1dc7791241
Sarveshwar Kumar Shukla <sarveshwarkumarshukla@gmail.com>
2025-07-02 00:40:23 +0530

    Refactor auth.js for improved readability and consistency in cookie handling

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,13 +2,14 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../module/user");
 const authRouter = express.Router();
-const {validatSignUpData} = require("../utils/validation");
+const { validatSignUpData } = require("../utils/validation");
 const jwt = require("jsonwebtoken");
 authRouter.post("/signUp", async (req, res) => {
   try {
     // validate the data
-    validatSignUpData(req)
-    const { firstName, lastName, email, password, skills, age, gender, about} = req.body;
+    validatSignUpData(req);
+    const { firstName, lastName, email, password, skills, age, gender, about } =
+      req.body;
     // Encrypt the Password
     const passwordHash = await bcrypt.hash(password, 10);
     const user = new User({
@@ -26,51 +27,64 @@ authRouter.post("/signUp", async (req, res) => {
     const savedUser = await user.save();
 
     const token = await savedUser.getJwt();
-    res.cookie("token", token, { expires: new Date(Date.now() + 900000), httpOnly : true});
-    
-    res.status(201).json({message : "data is added successfully....", data : savedUser});
+    // res.cookie("token", token, { expires: new Date(Date.now() + 900000), httpOnly : true});
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "Lax",
+      expires: new Date(Date.now() + 900000),
+    });
+
+    res
+      .status(201)
+      .json({ message: "data is added successfully....", data: savedUser });
   } catch (error) {
     res.status(400).send("Invalid Request " + error.message);
   }
 });
 
 authRouter.post("/login", async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email: email });
-  
-      if (!user) {
-        throw new Error("Invalid Credentials");
-      }
-  
-      const isPasswordValid = await user.validatePassword(password);
-  
-      if (!isPasswordValid) {
-        res.status(400).send("Invalid Credentials");
-      }
-       else {
-         const token = await user.getJwt();
-         // res.cookie("token", token,{ expires: new Date(Date.now() + 900000), httpOnly: true });
-         res.cookie("token", token, {
-           httpOnly: true,
-           secure: true, // secure only on HTTPS production
-           sameSite: "None", // or "Lax" depending on your needs
-           expires: new Date(Date.now() + 900000),
-         });
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
 
-         res.json(user);
-       }
-    } 
-    catch (error) {
-      res.status(400).send("Invalid Request");
+    if (!user) {
+      throw new Error("Invalid Credentials");
     }
-  });
 
-authRouter.post("/logout", async(req, res)=>{
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (!isPasswordValid) {
+      res.status(400).send("Invalid Credentials");
+    } else {
+      const token = await user.getJwt();
+      // res.cookie("token", token,{ expires: new Date(Date.now() + 900000), httpOnly: true });
+      // res.cookie("token", token, {
+      //   httpOnly: true,
+      //   secure: true, // secure only on HTTPS production
+      //   sameSite: "None", // or "Lax" depending on your needs
+      //   expires: new Date(Date.now() + 900000),
+      // });
+      res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "Lax",
+      expires: new Date(Date.now() + 900000),
+    });
+
+      res.json(user);
+    }
+  } catch (error) {
+    res.status(400).send("Invalid Request");
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
   res.cookie("token", null, {
-    expires: new Date(Date.now())
+    expires: new Date(Date.now()),
   });
   res.status(200).send("logout Successfully");
-})
+});
 
 module.exports = authRouter;
